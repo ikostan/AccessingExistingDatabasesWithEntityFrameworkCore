eda67290c4b3af1d69f7f828db0195a0ade912c9
ikostan <superegorius@gmail.com>
2017-12-03 15:27:27 -0800

    Order class >>> name anotations added [DisplayName("<name>")]

--- Project/EFClasses/Order.cs
+++ Project/EFClasses/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Project.EFClasses
 {
@@ -10,17 +11,37 @@ namespace Project.EFClasses
             OrderItem = new HashSet<OrderItem>();
         }
 
+        [DisplayName("Order Id")]
         public int OrderId { get; set; }
+
+        [DisplayName("Order Date")]
         public DateTime OrderDate { get; set; }
+
+        [DisplayName("Total Due")]
         public decimal? TotalDue { get; set; }
+
+        [DisplayName("Status")]
         public string Status { get; set; }
+
+        [DisplayName("Customer Id")]
         public int CustomerId { get; set; }
+
+        [DisplayName("Sales Person Id")]
         public int SalespersonId { get; set; }
+
+        [DisplayName("Created Date")]
         public DateTime CreatedDate { get; set; }
+
+        [DisplayName("Last Update")]
         public byte[] LastUpdate { get; set; }
 
+        [DisplayName("Order Has Items")]
         public ICollection<OrderItem> OrderItem { get; set; }
+
+        [DisplayName("Customer")]
         public Customer Customer { get; set; }
+
+        [DisplayName("Salesperson")]
         public Salesperson Salesperson { get; set; }
     }
 }
